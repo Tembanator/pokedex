9b646570fcf46b6380c5b738e16ea18a867de542
Tembanator <49640058+Tembanator@users.noreply.github.com>
2024-08-31 16:33:20 +0200

    Update Pokemon.jsx

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -34,20 +34,20 @@ export default function Pokemon() {
         <div className={`bg-${bgColor[0].color} w-full min-h-screen bg-opacity-65`}>
             <div className='mx-auto max-w-[500px] p-4'>
                 <div className='flex space-x-5 items-center text-white justify-center capitalize'>
-                    <IoArrowBack />
+                    <Link to={'/'}><IoArrowBack /></Link>
                     <h3 className='font-semibold md:text-xl'>{pokemon.name}</h3>
                     <p className='text-xs'>#{pokemon.id}</p>
                 </div>
 
                 {/* IMAGE WITH ARROWS */}
-                <div className="flex justify-center items-center space-x-5 text-white">
+                <div className="flex justify-center items-center space-x-5 text-white my-4">
                     <Link to={`/pokemon/${Number(id) > 1 ? Number(id) - 1 : '1'}`}><IoIosArrowBack /></Link>
                     <img src={pokemon.sprites?.front_default} alt={`${pokemon.name} sprite`} />
                     <Link to={`/pokemon/${Number(id) + 1}`}><IoIosArrowForward /></Link>
                 </div>
 
                 {/* INFO */}
-                <div className="bg-white rounded shadow-md w-full p-4">
+                <div className="bg-white rounded shadow-md w-full p-4 my-5">
                     <h3 className='font-semibold text-center'>About</h3>
                     <div className="flex justify-between">
                         <DetailTile icon={<TbWeight />} value={pokemon.weight} unit={'kg'} title={'weight'} />
@@ -55,7 +55,7 @@ export default function Pokemon() {
                         <DetailTile title={'move'} value={move} />
                     </div>
 
-                    <h3 className='font-semibold text-center'>Base Stats</h3>
+                    <h3 className='font-semibold text-center mt-8'>Base Stats</h3>
 
                     <div className='flex flex-col justify-center items-center space-y-2'>
                         {stats.map((stat, index) => <Statistic color={bgColor[0].color} statValue={stat} key={index} />)}
@@ -64,6 +64,6 @@ export default function Pokemon() {
 
 
             </div>
-        </div>
+        </div >
     )
 }
